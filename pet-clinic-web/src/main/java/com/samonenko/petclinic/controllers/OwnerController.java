49825cfff28e2cc54971c8cc023d1fedeab3d6ca
AnnaSamonenko <annsamonenko97@gmail.com>
2018-11-30 10:47:57 +0200

    Added templates of owner and controller for owner details

--- pet-clinic-web/src/main/java/com/samonenko/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/samonenko/petclinic/controllers/OwnerController.java
@@ -3,9 +3,13 @@ package com.samonenko.petclinic.controllers;
 import com.samonenko.petclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/owners")
 public class OwnerController {
 
     private final OwnerService ownerService;
@@ -14,15 +18,17 @@ public class OwnerController {
         this.ownerService = ownerService;
     }
 
-    @RequestMapping("/owners")
+    @RequestMapping("/")
     public String listOwners(Model model) {
         model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 
-    @RequestMapping("/owners/find")
-    public String findOwners() {
-        return "not-implemented";
+    @GetMapping("/{id}")
+    public ModelAndView ownersDetailsPage(@PathVariable Long id) {
+        ModelAndView mav = new ModelAndView("owners/ownerDetails");
+        mav.addObject(ownerService.findById(id));
+        return mav;
     }
 
 }
